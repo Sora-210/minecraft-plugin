d81b9a9f6b112a6a92c446caa0cc8101cdc0ab2d
Sora-210 <sokominto1231@gmail.com>
2025-06-19 23:08:40 +0900

    update: ログイン時にアイテム付与するように

--- MyPlugin/src/main/java/dev/sora210/minecraft_plugin/MyPlugin.java
+++ MyPlugin/src/main/java/dev/sora210/minecraft_plugin/MyPlugin.java
@@ -1,16 +1,30 @@
 package dev.sora210.minecraft_plugin;
 
+import org.bukkit.Material;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class MyPlugin extends JavaPlugin
+public class MyPlugin extends JavaPlugin implements Listener
 {
     @Override
     public void onEnable() {
+        getServer().getPluginManager().registerEvents(this, this);
+
         getLogger().info("自作プラグインが有効化されたよ！");
+
     }
 
     @Override
     public void onDisable() {
         getLogger().info("自作プラグインが無効化されたよ！");
+    }
+
+    @EventHandler
+    public void onPlayerJoin(org.bukkit.event.player.PlayerJoinEvent event) {
+        event.getPlayer().sendMessage("ようこそ、" + event.getPlayer().getName() + "さん！");
+        ItemStack diamond_sword = new ItemStack(Material.DIAMOND_SWORD);
+        event.getPlayer().getInventory().addItem(diamond_sword);
     }
 }
